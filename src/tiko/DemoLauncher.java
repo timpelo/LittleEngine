700b137665164f63daccf3410a6cd7505c075335
timpelo <janij.timonen@gmail.com>
2015-12-01 08:31:52 +0200

    Collider can now detect if they collide with other collider.

--- src/tiko/DemoLauncher.java
+++ src/tiko/DemoLauncher.java
@@ -6,10 +6,10 @@ public class DemoLauncher {
 
     public static void main(String[] args) {
         final int WINDOW_HEIGHT = 720;
-        final int WINDOW_WIDHT = 1280;
+        final int WINDOW_WIDTH = 1280;
 
         DemoGame game = new DemoGame();
         ScreenManager scrMgr = new ScreenManager(2000, 1000,
-                WINDOW_WIDHT, WINDOW_HEIGHT, game);
+                WINDOW_WIDTH, WINDOW_HEIGHT, game);
     }
 }
